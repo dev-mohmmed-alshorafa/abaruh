32353faabd2afd08ae840cf20b65217e666670a1
mohmmedalshorafa1996 <mohmmedalshorafa1996@gmail.com>
2023-02-01 15:41:44 +0200

    keep going

--- src/components/signup/SelectNum.jsx
+++ src/components/signup/SelectNum.jsx
@@ -1,20 +1,18 @@
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
 import { useState } from 'react'
-
 function SelectNum() {
-
   const [value, setValue] = useState()
   return (
-    <div className='select-num'>
-
-    <PhoneInput
-    international
-    defaultCountry="RU"
-    value={value}
-    onChange={setValue}/>
-        </div>
-
+    <div className="select-num">
+      <PhoneInput
+        international
+        defaultCountry="SA"
+        value={value}
+        onChange={setValue}
+        countries={['SA']}
+      />
+    </div>
   )
 }
 export default SelectNum
